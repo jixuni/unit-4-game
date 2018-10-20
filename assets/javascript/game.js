b5ee30434b31980efa3dc96c2e2f6c2813a3fc20
Jixu NI <40869621+jixuni@users.noreply.github.com>
2018-10-20 00:13:00 -0400

    fixed various bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,33 @@
-// 1. need to complete layout for Pokemon game
-// 2. need 4 playable pokemon 
-// 3. Have each pokemon with different attribute, hp, atk power, atk power counter
-// 4. After choosing pokemon select another pokemon to battle
-// 5. The player pokemon will increase by x += x each tern
-// 6. After defeating one, choose another to to battle, but the player keeps that attack strenght
-// 7. End game when all pokemon are defeated
-// 8. possibly add music to sound attach to teach pokemon
 
-
-var paladin = {
-    occupation: "Paladin",
-    healthPoints: 300,
-    attackPower: 15,
+var charList = {
+    paladin: {
+    occupation: "paladin",
+    healthPoints: 120,
+    attackPower: 8,
     counterAttack: 15,
-}
+    },
 
-var mage = {
+    mage: { 
     occupation: "mage",
-    healthPoints: 150,
-    attackPower: 30,
-    counterAttack: 30,
-}
+    healthPoints: 100,
+    attackPower: 14,
+    counterAttack: 5,
+    },
 
-var ranger = {
+    ranger: {
     occupation: "ranger",
-    healthPoints: 200,
-    attackPower: 20,
+    healthPoints: 150,
+    attackPower: 8,
     counterAttack: 20,
-}
+    },
 
-var berserker = {
+    berserker: {
     occupation: "berserker",
-    healthPoints: 250,
-    attackPower: 25,
+    healthPoints: 180,
+    attackPower: 7,
     counterAttack: 25,
+    },
 }
-
 
 
 
@@ -43,75 +35,93 @@ var currentCharacter;
 var currentEnemy;
 var $pal = $("#paladin"); 
 var $mage = $("#mage");
+var $berserker = $("#berserker");
+var $ranger = $("#ranger");
 var attackPowerCounter; 
+var winCounter = 0;
+var keys = Object.keys(charList);
+var len = keys.length;
+keys.length
 
-$pal.on("click", function(){
-    currentCharacter = paladin;
-    $pal.css("-webkit-transform", "scaleX(-1)");
-    $pal.css("transform", "scaleX(-1)");
-    $(".initialPick").text("Pick someone to rumble with!");
-    //console.log("you pick paladin");
 
+$(".character").on("click", function(){
+    if(currentCharacter === undefined && $(this).attr("id") == "paladin"){
+        currentCharacter = charList.paladin;
+        $(this).css("transform", "scaleX(-1)");
+        
+    } else if(currentCharacter === undefined && $(this).attr("id") == "berserker"){
+        currentCharacter = charList.berserker;
+        $(this).css("transform", "scaleX(-1)");
+        
+    } else if(currentCharacter === undefined && $(this).attr("id") == "mage"){
+        currentCharacter = charList.mage;
+        $(this).css("transform", "scaleX(-1)");
+        
+    } else if(currentCharacter === undefined && $(this). attr("id") == "ranger"){
+        currentCharacter = charList.ranger;
+        $(this).css("transform", "scaleX(-1)");
+ 
+    }
+    battle();
 })
 
-$mage.on("click", function(){
-    currentEnemy = mage;
-    $mage.css("-webkit-transform", "scaleX(-1)");
-    $mage.css("transform", "scaleX(-1)");
-    $(".initialPick").text("Your opponent is mage");
-})
+var currentSelect = $(".character").on("click", function() {
+     currentSelect = $(this).attr("id")
+     $(this).css("transform", "scaleX(-1)")
+    });
+
+
+function battle (){
+     
+    switch(currentSelect) {
+        case "mage":
+            if(currentSelect !== currentCharacter.occupation){
+                currentEnemy = charList.mage;
+                console.log(currentEnemy);
+            } 
+        break;
+        case "paladin":
+            if(currentSelect !== currentCharacter.occupation){
+                currentEnemy = charList.paladin;
+                console.log(currentEnemy);
+            } 
+        break;
+        case "berserker":
+            if(currentSelect !== currentCharacter.occupation){
+                currentEnemy = charList.berserker;
+                console.log(currentEnemy);
+            } 
+        break;
+        case "ranger":
+            if(currentSelect !== currentCharacter.occupation){
+                currentEnemy = charList.ranger;
+                console.log(currentEnemy);
+            } 
+        break;
+    }   
+
+}
+
 
 $(".attack").on("click", function(){
-    
+    $(".attackSound").get(0).play()
+    $(this).css({"transform": "translateX(50px)"});
     currentEnemy.healthPoints -= currentCharacter.attackPower;
     currentCharacter.healthPoints -= currentEnemy.attackPower;
     $(".initialPick").html("You hit enemy for " + currentCharacter.attackPower+"\nEnemy health " + currentEnemy.healthPoints + "\nEnemy hit your for " + currentEnemy.attackPower + "\nYour hp " + currentCharacter.healthPoints);
-    //console.log("You hit enemy for " + currentCharacter.attackPower);
-    //console.log("enemy health " + currentEnemy.healthPoints);
-    //console.log("Enemy hit your for " + currentEnemy.attackPower)
-    //console.log("Your hp " + currentCharacter.healthPoints);
+    
 
     currentCharacter.attackPower += 15;
     if(currentEnemy.healthPoints <= 0 && currentCharacter.healthPoints > 0){
-        $(".attack").prop("disabled",true);
         $(".initialPick").text("Pick another opponent to battle!!");
-        
-        
+        winCounter++; 
+        currentSelect = undefined;
+        battle();   
+    }     
+    if(currentCharacter.healthPoints <= 0 || winCounter === len-1 ) {
+        $(".initialPick").text("GAME OVER!!");
+        $(".attack").prop("disabled",true);
     }
-    
-    
     
 })
 
-
-
-
-
-
-
-
-
-/* var game = {
-    activeGame: false,
-    currentCharacter: 0,
-    currentEnemey: 0,
-    newGame: function() {
-        this.activeGame = true;
-        var $pal = $("#paladin"); 
-        var $mage = $("#mage");
-
-        pal.on("click", function(){
-            currentCharacter = paladin;
-            $pal.css("-webkit-transform", "scaleX(-1)");
-            $pal.css("transform", "scaleX(-1)");
-            $(".initialPick").text("Pick someone to rumble with!")
-            //console.log("you pick paladin");
-        })
-        
-        $mage.on("click", function(){
-            currentEnemey = mage;
-        })
-    }
-}
-
-game.newGame(); */
